9c8e999ef63c364cd4174f02bbebf7a21e42f5ad
Rodolfo Navalon <navalonrodolfo@gmail.com>
2018-01-23 21:46:54 -0500

    Updated the debug class

--- app/src/main/kotlin/com/rodolfonavalon/canadatransit/controller/util/DebugUtil.kt
+++ app/src/main/kotlin/com/rodolfonavalon/canadatransit/controller/util/DebugUtil.kt
@@ -10,59 +10,43 @@ object DebugUtil {
     /**
      * assertFalse
      */
-    fun assertFalse(assertion: Boolean) {
-        assert(!assertion)
-    }
-
-    /**
-     * assertFalse
-     */
-    fun assertFalse(assertion: Boolean, lazyMessage: () -> Any) {
-        assert(!assertion, lazyMessage)
+    fun assertFalse(assertion: Boolean, message: String? = null) {
+        assert(!assertion, message)
     }
 
     /**
      * assertTrue
      */
-    fun assertTrue(assertion: Boolean) {
-        assert(assertion)
+    fun assertTrue(assertion: Boolean, message: String? = null) {
+        assert(assertion, message)
     }
 
     /**
-     * assertTrue
+     * assertEqual
      */
-    fun assertTrue(assertion: Boolean, lazyMessage: () -> Any) {
-        assert(assertion, lazyMessage)
+    fun assertEqual(first: Any, second: Any, message: String? = null) {
+        assert(first === second, message)
     }
 
     /**
      * assertMainThread
      */
     fun assertMainThread() {
-        if (Looper.myLooper() != Looper.getMainLooper()) {
-            val message = "Method should be executed within the Main Thread!!"
-            throw AssertionError(message)
-        }
+        assertTrue(Looper.myLooper() == Looper.getMainLooper(),
+                "Method should be executed within the Main Thread!!")
     }
 
     /**
      * assertWorkerThread
      */
     fun assertWorkerThread() {
-        if (Looper.myLooper() == Looper.getMainLooper()) {
-            val message = "Method should be executed within the Worker Thread!!"
-            throw AssertionError(message)
-        }
+        assertFalse(Looper.myLooper() == Looper.getMainLooper(),
+                "Method should be executed within the Worker Thread!!")
     }
 
-    private fun assert(value: Boolean) {
-        assert(value) { "Assertion Failed" }
-    }
-
-    private inline fun assert(value: Boolean, lazyMessage: () -> Any) {
+    private fun assert(value: Boolean, message: String? = null) {
         if (!value) {
-            val message = lazyMessage()
-            throw AssertionError(message)
+            throw AssertionError(message ?: "Assertion Failed")
         }
     }
 
