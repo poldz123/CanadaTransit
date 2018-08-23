a9063dcbe483e080c5020cfec5c7a740b4bd4203
Rodolfo Navalon <navalonrodolfo@gmail.com>
2018-08-23 01:41:14 -0400

    Added script for test

---
+++ scripts/unit-test.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+# This script will run the unit test for the application
+# TODO: Make this test to be runnable in small and full mode.
+#       small = Makes the test smaller and run only the most important tests
+#       full = Runs all of the test for the application.
+./gradlew :app:testDebugUnitTest
